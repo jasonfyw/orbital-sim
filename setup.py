4972b3538288e853b0e0e8fe66a0c8e493ac1d44
Jason Wang <jasonwang0610@gmail.com>
2020-07-14 22:25:29 +0200

    Fixed encoding error

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ from setuptools import setup, find_packages
 HERE = pathlib.Path(__file__).parent
 
 # The text of the README file
-README = (HERE / "README.md").read_text()
+README = (HERE / "README.md").read_text(encoding = 'utf-8', errors = 'ignore')
 
 setup(
     name="orbital-sim",
-    version="0.9.2",
+    version="0.9.3",
     description="A simple physics engine build over a PyGame simulation to model planetary orbits in space",
     long_description=README,
     long_description_content_type="text/markdown",
@@ -25,5 +25,5 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=["pygame", "astropy", "astroquery"],
-    download_url="https://github.com/jasonfyw/orbital-sim/archive/v0.9.1.tar.gz"
+    download_url="https://github.com/jasonfyw/orbital-sim/archive/v0.9.2.tar.gz"
 )
